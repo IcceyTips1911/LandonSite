4328b9d199e09eb9226d2a29d4db6e171b380e3e
IcceyTips1911 <landon.johnson1911@gmail.com>
2026-08-27 13:41:03 -0500

    Finished information for about page and now workign on the setup for contact page to enable emails to myself

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css2/style.css">
+    <link rel="stylesheet" href="./css2/mobile.css">
+    <title>Contact Me</title>
+</head>
+<body>
+    <div class="header">
+        <h1 class="logo">
+            <a href="landon.html">Landon's Portfolio</a>
+        </h1>
+
+        <div class="nav-container">
+            <a href="about.html">About</a>
+            <a href="#">Portfolio</a>
+            <a href="contact.php">Contact</a>
+        </div>
+        <script src="/json/active.js"></script>
+    </div>
+</body>
+</html>
